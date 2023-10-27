f3017c160ecc0444be5131ee2f24c3f91db9aade
gianellarobles <gianellarobles@gmail.com>
2023-10-26 21:51:56 -0400

    new client folder by creating a vue folder

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,34 +1,23 @@
-import { createRouter, createWebHashHistory, type NavigationGuardNext, type RouteLocation, } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import { getSession } from '@/model/session';
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        { path: "/login", name: "login", component: LoginView },
-        {
-            path: "/about",
-            name: "about",
-            component: () => import("../views/AboutView.vue"),
-            beforeEnter: requireLogin,
-        },
-    ]
-});
-
-function requireLogin(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
-    const session = getSession();
-    if (!session.user) {
-        session.refirectUrl = to.fullPath;
-        next('/login');
-    } else {
-        next();
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
-}
+  ]
+})
 
 export default router
