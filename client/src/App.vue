ef6f7e85043fd710348fcb546d0945dc006c982d
gianellarobles <gianellarobles@gmail.com>
2023-11-06 13:16:05 -0500

    fixing the website
    adding fontaweseome and bulma into dependencies in package.json. Altering the home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+     <i class="fas fa-hand-holding-heart"></i>
+    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    
+      <HelloWorld msg="HealingFitness"/>
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/home">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
