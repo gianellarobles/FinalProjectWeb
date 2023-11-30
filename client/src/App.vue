beeb8860e0f3c071548b5f80df69af436e73eeee
gianellarobles <gianellarobles@gmail.com>
2023-11-30 13:25:04 -0500

    took out unnecessary items

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,8 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
      
     <!-- This is the logo for HealingFitness-->
     <div class="wrapper">
-
-    
+  
       <HelloWorld msg="Healing Fitness"/>
   
     </div>
@@ -22,57 +21,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 header {
-  line-height: 1;
+  line-height: 3.5;
   max-height: 120vh;
-}
-
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-   
-  }
-
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
