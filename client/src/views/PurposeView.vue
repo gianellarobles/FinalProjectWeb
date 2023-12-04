5ad89f8b0c71144f4cdbfb519b1f5c249416bbde
gianellarobles <gianellarobles@gmail.com>
2023-12-04 13:07:46 -0500

    PurposeView
    official Purpose View done

--- client/src/views/PurposeView.vue
+++ client/src/views/PurposeView.vue
@@ -12,11 +12,18 @@
 
     <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;In the beginning of this year, I found out I had a health issue that led to changing my eating habits. I was not aware that I was lacking protein and consuming a high amount of carbohydrates. As a result, I had to meet with multiple doctors and go through different check-ups. They told me that I had a fatty liver, which was concerning to have in my early-twenties. As a result, I started seeing a nutritionist and she helped me come up with well-balanced meals. </h3>
     <br>
-    <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;Some changes I made was cutting out gluten and lowering my carb intake. Additionally, I started looking at ingredients and the nutrition fact label to all my foods. I noticed that certain foods had high sugar and high carbs so I tried to limit my amount. </h3>
+    <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;Some changes I made was cutting out gluten and lowering my carb intake. Additionally, I started looking at ingredients and the nutrition fact label to all my foods. I noticed that certain foods had high sugar and high carbs so I tried to limit my amount for both. </h3>
      <br>
             <img src="68764250488__504B42B1-F9B5-4793-8358-102DDCE5C226.png" alt="Food" class="center">
-            <br>
+         <br>
+    <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;After changing my eating habits, I started going to the gym. I would walk and try lifting weight. The issue slowly became an obsession with keeping this strict diet and trying to go to the gym most days. There would be days I would feel gulity for not going to the gym or craving a cookie after eating my meals. </h3>
 
+    <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;Thankfully, I was spekaing to my therapist about this unhealthy relationship with food and exercise. She helped me realize that I was being too hard on myself and that even small progress is good. I was so focused on my health that I was not enjoying the food I was eating or the time I was spending at the gym. At that moment I realized, mental health is just as important as physical health when changing your lifestyle. </h3>
+       <br>
+                <img src="health.png" alt="Mental health" class="center">
+             <br>
+
+    <h3 class="has-text-weight-medium has-text-centered">&nbsp;&nbsp;I decided to create this website to help others who are going through a similar situation. I want to encourage people to have a positive relationship with food and exercise. I want to help people realize that it is okay to have a cookie or a slice of pizza. I want people realize that it is okay to take a rest day and not go to the gym. Progress is not the same for everyone. Just because someone goes to the gym everyday and you aren't, does not take away from the progess that you have done so far. With Healing Fitness, I hope everyone sees fitness in a different light and start appreicate the beginning journey of your new fitness lifestyle in a healthy way.</h3>
   </template>
 
 <style>
