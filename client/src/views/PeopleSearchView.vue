24a3bb5ea3f37e63806c05a75878ca789738e64a
gianellarobles <gianellarobles@gmail.com>
2023-12-05 18:13:38 -0500

    Final SearchPage

--- client/src/views/PeopleSearchView.vue
+++ client/src/views/PeopleSearchView.vue
@@ -1,23 +1,82 @@
 <template>
 
  <div class="search">
-        <h2>
-            
-            This is my search page
-        </h2>
-    </div>
-
-    <p><strong>Enter name</strong></p>
-    <input class="input is-danger" type="text" placeholder="Danger input">
-    
+      <br>
+      <br>
+    <p><strong>Enter Username</strong></p>
+     <div class="field is-grouped">
+          <p class="control is-expanded has-icons-left has-icons-right">
+            <input class="input is-danger" type="Username" placeholder="Username">
+            <span class="icon is-small is-left">
+              <i class="fas fa-search"></i>
+            </span>
+          </p>
+          <p class="control">
+            <a class="button is-danger has-text-weight-semibold">
+              Search
+              </a>
+          </p>
+        </div>
+        <br>
+         <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-96x96">
+              <img class="is-rounded" src="Cute-PNG-Pic.png" alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Angela Oaks</strong> <small>@angieOaks</small>
+                <br>
+                Hi, I am Angie and I am looking for a workout buddy!
+        
+               <h4 class="is-size-6 has-text-weight-semibold">Status: 
+                <span class="icon has-text-warning">
+                    <i class="fas fa-smile-beam"></i>
+                 </span> 
+                </h4> 
+           </p>
+        </div>
+          </div>
+        </article>
+      </div>
+        <div class="box">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-96x96">
+                  <img class="is-rounded" src="E2yj-NxXIAE1T3h.png" alt="Image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                     <strong>John Smith</strong> <small>@johnsmith</small> 
+                    <br>
+                    Hi, I am John and I love hikes.
+        
+                   <h4 class="is-size-6 has-text-weight-semibold">Status: 
+                    <span class="icon has-text-warning">
+                        <i class="far fa-meh-rolling-eyes"></i>
+                     </span> 
+                    </h4> 
+               </p>
+            </div>
+              </div>
+            </article>
+        </div>
+   </div>
 </template>
 
 <style>
 @media (min-width: 640px) {
     .search {
-        min-height: 100vh;
+        min-height: 120vh;
         display: flex;
         flex-direction: column;
+        margin: 0 auto;
+        width: 75%;
     }
 }
 </style>
