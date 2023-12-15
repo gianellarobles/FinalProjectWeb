a59fa069f7ab41d7c7ee99d36362400ef89c0342
gianellarobles <gianellarobles@gmail.com>
2023-12-14 19:09:57 -0500

    all backend

--- server/index.js
+++ server/index.js
@@ -1,18 +1,48 @@
-// express.js
-
-const path = require('path');
+const path = require('path')
 const express = require('express');
+require('dotenv').config();
+//const productController = require('./controllers/products');
 const userController = require('./controllers/users');
-const activityController = require('./controllers/activity');
-
-//needs to add a parseAuthorizationToken functtion from the middleware folder into the authorization.js file 
+const { parseAuthorizationToken, requireUser } = require('./middleware/authorization');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+console.log(`The best class at SUNY New Paltz is ${process.env.BEST_CLASS}`);
+
+app
+    .use('/', express.static(path.join( __dirname, '../client/dist/') ) )
+    .use(express.json())
+
+    // CORS
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+        if(req.method === 'OPTIONS') {
+            return res.send(200);
+        }
+        next();
+    })
+
+    .use(parseAuthorizationToken)
+
+    //.use('/api/v1/products', requireUser(), productController)
+    .use('/api/v1/users', userController)
+
+    .get('*', (req, res) => {
+        res.sendFile(path.join( __dirname, '../client/dist/index.html') )
+    });
+
+app
+    .use((err, req, res, next) => {
+        console.error(err);
+        res
+            .status(err?.status || 500)
+            .json({ message: err?.message || err });
+    })
+
+
 
 console.log('1: Trying to start server...');
 
@@ -20,15 +50,4 @@ app.listen(PORT, () => {
     console.log(`2: Server is running at http://localhost:${PORT}`);
 });
 
-//mobile menu
-//This is where we add the app version and displayed the tabs active 
-    //Need to be changed up a little because it has to be functional, not just opened already 
-
 console.log('3: End of file, waiting for requests...');
-
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
-});
